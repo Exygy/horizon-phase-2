8aca125a8fcceefc57262893c5a95806ceb6e225
Pierre Hunault <pierre@exygy.com>
2019-12-11 22:07:46 -0800

    support favicon

--- server/horizon/urls.py
+++ server/horizon/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import include, path, re_path
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import RedirectView
 
 from .views import FrontendAppView, GraphQLView
 
@@ -9,5 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=settings.GRAPHQL_DEBUG))),
     path('gql', csrf_exempt(GraphQLView.as_view())),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
     re_path(r'.*', FrontendAppView.as_view()),
 ]
