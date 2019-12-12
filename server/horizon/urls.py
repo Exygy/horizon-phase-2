3c4ff0e81b10f4c3aa4e8ed547db1f3f936e92ab
Pierre Hunault <pierre@exygy.com>
2019-12-11 22:16:42 -0800

    remove redirect for favicon

--- server/horizon/urls.py
+++ server/horizon/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import RedirectView
 
 from .views import FrontendAppView, GraphQLView
 
@@ -10,6 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=settings.GRAPHQL_DEBUG))),
     path('gql', csrf_exempt(GraphQLView.as_view())),
-    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
     re_path(r'.*', FrontendAppView.as_view()),
 ]
